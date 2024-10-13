62f21b9c1c589507cfcf2a05e453e6f68ae5b65f
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2024-10-13 17:25:59 +0200

    selectFilteredContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
 import {
-  selectContacts,
   selectError,
   selectFilteredContacts,
   selectLoading,
 } from "../../redux/contactsSlice";
-// import { selectNameFilter } from "../../redux/filtersSlice";
-import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contactsOps";
 
 const ContactList = () => {
   const loading = useSelector(selectLoading);
